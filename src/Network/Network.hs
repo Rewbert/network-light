e38456b42b011cfcfd3bdb7a364332bb50ee2587
Lennart Augustsson <lennart@augustsson.net>
2025-09-29 16:55:29 +0200

    Lots of little cleanups.

--- src/Network/Network.hs
+++ src/Network/Network.hs
@@ -5,7 +5,6 @@ Most functions only exist as IO () variants, where in reality they might fail wi
 These variants can be trivially added.
 
 -}
-{-# LANGUAGE CPP #-}
 module Network.Network where
 
 import Data.Char
@@ -21,17 +20,13 @@ import Foreign.Marshal.Array
 import Foreign.Marshal.Utils
 import Foreign.C.Error
 
-#if defined(__GLASGOW_HASKELL__)
-type CSSize = CInt
-#endif
-
 foreign import ccall "sys/socket.h socket"  c_socket        :: CInt -> CInt -> CInt -> IO CInt
 foreign import ccall "sys/socket.h connect" c_connect       :: CInt -> Ptr SockAddr -> CInt -> IO CInt
 foreign import ccall "sys/socket.h bind"    c_bind          :: CInt -> Ptr SockAddr -> CInt -> IO CInt
 foreign import ccall "sys/socket.h accept"  c_accept        :: CInt -> Ptr SockAddr -> Ptr CInt -> IO CInt
 foreign import ccall "sys/socket.h listen"  c_listen        :: CInt -> CInt -> IO CInt
-foreign import ccall "sys/socket.h send"    c_send          :: CInt -> Ptr Word8 -> CSize -> CInt -> IO CSSize
-foreign import ccall "sys/socket.h recv"    c_recv          :: CInt -> Ptr Word8 -> CSize -> CInt -> IO CSSize
+foreign import ccall "sys/socket.h send"    c_send          :: CInt -> Ptr Word8 -> CSize -> CInt -> IO CInt
+foreign import ccall "sys/socket.h recv"    c_recv          :: CInt -> Ptr Word8 -> CSize -> CInt -> IO CInt
 foreign import ccall "sys/socket.h setsockopt" c_setsockopt :: CInt -> CInt -> CInt -> Ptr Word8 -> CInt -> IO CInt
 
 foreign import ccall "unistd.h     close"   c_close   :: CInt -> IO CInt
@@ -46,7 +41,7 @@ data Domain
 
 instance Enum Domain where
     toEnum 2 = AF_INET
-    toEnum _ = error "not added yet"
+    toEnum _ = error "Domain.toEnum unimplemented"
 
     fromEnum AF_INET = 2
 
@@ -59,7 +54,7 @@ data StreamType
 instance Enum StreamType where
     toEnum 1 = SOCK_STREAM
     toEnum 2 = SOCK_DGRAM
-    toEnum _ = error "not added yet"
+    toEnum _ = error "StreamType.toEnum unimplemented"
 
     fromEnum SOCK_STREAM = 1
     fromEnum SOCK_DGRAM = 2
@@ -80,15 +75,15 @@ data SockOpt
 
 instance Enum SockOpt where
     toEnum 2 = SO_REUSEADDR
-    toEnum _ = error "not added yet"
+    toEnum _ = error "SockOpt.toEnum unimplemented"
 
     fromEnum SO_REUSEADDR = 2
 
 -- | Create a new socket, throwing an exception if creation failed
 socket :: Domain -> StreamType -> IO Socket
 socket d st = do
-    i <- throwErrnoIfMinus1 "error allocating socket" $
-             c_socket (CInt $ fromIntegral $ fromEnum d) (CInt $ fromIntegral $ fromEnum st) 0
+    i <- throwErrnoIfMinus1 "socket" $
+             c_socket (cFromEnum d) (cFromEnum st) 0
     return (Socket i)
 
 -- | Set a socket option
@@ -97,70 +92,58 @@ setsocketopt (Socket socketfd) so = do
     -- please see the C setsocketopt to see what the parameters are, if it is unclear
     -- (it was unclear to me first)
     with (1 :: Word8) $ \p -> do
-        _ <- throwErrnoIfMinus1 "error in setsocketopt" $ do
-                c_setsockopt socketfd (CInt 1 {- SOL_SOCKET = 1 -}) (CInt $ fromIntegral $ fromEnum so) p (CInt $ fromIntegral (sizeOf (undefined :: CInt)))
-        return ()
+        throwErrnoIfMinus1_ "setsocketopt" $
+            c_setsockopt socketfd (CInt 1 {- SOL_SOCKET = 1 -}) (cFromEnum so) p (cSizeOf (0 :: CInt))
 
 -- | Close a socket
 close :: Socket -> IO ()
 close (Socket fd@(CInt fd')) = do
-    _ <- throwErrnoIfMinus1 ("error closing socket " ++ show fd') $ do
-            c_close fd
-    return ()
+    throwErrnoIfMinus1_ ("close socket " ++ show fd') $ do
+        c_close fd
 
 -- | Establish a connection
 connect :: Socket -> SockAddr -> IO ()
 connect (Socket socketfd) sockaddr =
-    with sockaddr $ \p -> do
-        _ <- throwErrnoIfMinus1 "error in connect" $ do
-                c_connect socketfd p (CInt $ fromIntegral $ sizeOf sockaddr)
-        return ()
+    with sockaddr $ \p ->
+        throwErrnoIfMinus1_ "connect" $ do
+            c_connect socketfd p (cSizeOf sockaddr)
 
 -- | Establish a connection, returning a Bool to indicate whether the action was successful
 connect' :: Socket -> SockAddr -> IO Bool
 connect' (Socket socketfd) sockaddr =
     with sockaddr $ \p -> do
-        CInt e <- c_connect socketfd p (CInt $ fromIntegral $ sizeOf sockaddr)
+        CInt e <- c_connect socketfd p (cSizeOf sockaddr)
         return $ e >= 0
 
 -- | Bind a socket to a SockAddr
 bind :: Socket -> SockAddr -> IO ()
 bind (Socket socketfd) sockaddr =
     with sockaddr $ \p -> do
-        _ <- throwErrnoIfNegative "error in bind" $ do
-                c_bind socketfd p (CInt (fromIntegral $ sizeOf sockaddr))
-        return ()
+        throwErrnoIfMinus1_ "bind" $ do
+            c_bind socketfd p (cSizeOf sockaddr)
 
 -- | Accept an incoming connection request
 accept :: Socket -> IO (Socket, SockAddr)
 accept (Socket server_fd) = do
     allocaBytes (sizeOf (undefined :: SockAddr)) $ \p ->
-        allocaBytes 4 $ \p_size -> do
-            poke p_size 16
-
-            CInt e <- throwErrnoIfNegative "error in accept" $
-                c_accept server_fd p p_size
+        with (cSizeOf (undefined :: SockAddr)) $ \p_size -> do
+            s <- throwErrnoIfMinus1 "accept" $
+                     c_accept server_fd p p_size
             sockaddr <- peekSockAddr p
-            return (Socket (CInt e), sockaddr)
+            return (Socket s, sockaddr)
 
 -- | Set a socket to listening mode
 listen :: Socket -> IO ()
 listen (Socket df) = do
-    _ <- throwErrnoIfNegative "error in listen" $
-            c_listen df (CInt 1)
-    return ()
+    throwErrnoIfMinus1_ "listen" $
+        c_listen df (CInt 1)
 
 -- | Send a buffer of data over a socket
 sendBuf :: Socket -> Ptr Word8 -> Int -> IO Int
 sendBuf (Socket socketfd) buf len =
-    throwErrnoIfMinus1 "error in send" $ do
-#if defined(__GLASGOW_HASKELL__)
-        CInt e <-
-#else
-        e <-
-#endif
-            c_send socketfd buf (CSize (fromIntegral len)) (CInt 0)
-        return $ fromIntegral e
+    throwErrnoIfMinus1 "send" $ do
+        CInt e <- c_send socketfd buf (CSize $ fromIntegral len) (CInt 0)
+        return e
 
 -- | Convenience function to transmit a String over a socket
 sendString :: Socket -> String -> IO Int
@@ -172,14 +155,9 @@ sendString sock str =
 -- | Receive data over a socket
 recvBuf :: Socket -> Ptr Word8 -> Int -> IO Int
 recvBuf (Socket socketfd) buf len = do
-    throwErrnoIfMinus1 "error in recv" $ do
-#if defined(__GLASGOW_HASKELL__)
-        CInt e <-
-#else
-        e <-
-#endif
-            c_recv socketfd buf (CSize (fromIntegral len)) (CInt 0)
-        return $ fromIntegral e
+    throwErrnoIfMinus1 "recv" $ do
+        CInt e <- c_recv socketfd buf (CSize $ fromIntegral len) (CInt 0)
+        return e
 
 -- | Convenience function to receive a string (of a maximum length)
 recvString :: Socket -> Int -> IO String
@@ -194,13 +172,6 @@ instance Storable SockAddr where
     peek = peekSockAddr
     poke = pokeSockAddr
 
-callocaBytes :: Int -> (Ptr a -> IO b) -> IO b
-callocaBytes s f = do
-    p <- callocBytes s
-    b <- f p
-    free p
-    return b
-
 -- | Write a SockAddr to a buffer
 pokeSockAddr :: Ptr SockAddr -> SockAddr -> IO ()
 pokeSockAddr p (SockAddrInet port address) =
@@ -213,9 +184,8 @@ pokeSockAddr p (SockAddrInet port address) =
     -- the port in network order
     sin_port :: [Word8]
     sin_port =
-        let w16 = fromIntegral port :: Word16
-            high = fromIntegral ((w16 `shiftR` 8) .&. 0xFF)
-            low = fromIntegral (w16 .&. 0xFF)
+        let high = fromIntegral ((port `shiftR` 8) .&. 0xFF)
+            low  = fromIntegral (port .&. 0xFF)
         in [high, low]
     
     -- the address (e.g. "127.0.0.1" -> [127,0,0,1] :: [Word8])
@@ -232,22 +202,17 @@ pokeSockAddr p (SockAddrInet port address) =
 
 -- | Read a SockAddr from a buffer
 peekSockAddr :: Ptr SockAddr -> IO SockAddr
-peekSockAddr p =
-    let p' :: Ptr Word8
-        p' = castPtr p
-    in do
-        xs <- peekArray 8 p'
-        case xs of
-            _:_:high:low:sin_addr -> do
-                let w16     =  (fromIntegral high `shiftL` 8) .|. fromIntegral low :: Word16
-                    port    = fromIntegral w16 :: Int
-                    address = intercalate "." $ map show sin_addr
-                return $ SockAddrInet port address
-            _ -> error "somehow wrong format lmao"
+peekSockAddr p = do
+    xs <- peekArray 8 (castPtr p :: Ptr Word8)
+    case xs of
+        _:_:high:low:sin_addr -> do
+            let port    = (fromIntegral high `shiftL` 8) .|. fromIntegral low
+                address = intercalate "." $ map show sin_addr
+            return $ SockAddrInet port address
+        _ -> undefined
 
-throwErrnoIfNegative :: (Ord a, Num a) => String -> IO a -> IO a
-throwErrnoIfNegative str act = do
-    r <- act
-    if r < 0
-        then error str
-        else return r
+cSizeOf :: (Storable a) => a -> CInt
+cSizeOf = CInt . sizeOf
+
+cFromEnum :: (Enum a) => a -> CInt
+cFromEnum = CInt . fromEnum
