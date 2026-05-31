83a599de082ca3c805b2910dc39285417989bfc8
Lennart Augustsson <lennart@augustsson.net>
2026-05-31 18:31:16 +0200

    A simple client/server example.

--- examples/Client.hs
+++ examples/Client.hs
@@ -1,12 +1,16 @@
 module Client where
 import System.Network
+import System.Environment
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let msg | null args = "Hello"
+          | otherwise = unwords args
   fd <- socket AF_INET SOCK_STREAM
   connect fd (mkSockAddr 9900 (Just "127.0.0.1"))
-  putStrLn "Sending"
-  sendString fd "Hello"
+  putStrLn $ "Sending: " ++ msg
+  sendString fd msg
   str <- recvString fd 1000
   putStrLn $ "Got: " ++ show str
   close fd
--- examples/Server.hs
+++ examples/Server.hs
@@ -6,7 +6,7 @@ main = do
   sfd <- socket AF_INET SOCK_STREAM
   setsocketopt sfd SO_REUSEADDR 1
   bind sfd (mkSockAddr 9900 Nothing)
-  listen sfd
+  listen sfd 2
   serve sfd
 
 serve :: Socket -> IO ()
